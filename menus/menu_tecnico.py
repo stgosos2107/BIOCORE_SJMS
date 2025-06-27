fad6d4c03f84eabcd378461ff7fb50deea06b16b
sofia0911 <sofia.hosorio@udea.edu.co>
2025-06-27 10:34:42 -0500

    Update menu_tecnico.py

--- menus/menu_tecnico.py
+++ menus/menu_tecnico.py
@@ -1,8 +1,8 @@
 #Se realiza la importación de las funciones necesarias para el funcionamiento del menu del técnico
 
 from funciones.reportes_mongo import subir_reporte_tecnico, consultar_reportes
-from funciones.mantenimientos import registrar_mantenimiento_preventivo,registrar_mantenimiento_correctivo
-from funciones.equipos import ver_equipos_asignados
+from funciones.mantenimientos import registrar_mantenimeinto
+from funciones.equipos import ver_equipos, actualizar_equipo 
 
 #Este menu permite al técnico realizar diversas acciones relacionadas con el mantenimiento de equipos y la gestión de reportes técnicos.
 def menu_tecnico(tecnico_id):
@@ -10,8 +10,8 @@ def menu_tecnico(tecnico_id):
     while True:
         print("\n--- Menú Técnico ---")
         print("1. Ver equipos asignados")
-        print("2. Registrar mantenimiento preventivo")
-        print("3. Registrar mantenimiento correctivo")
+        print("2. Actualizar estado de un equipo")
+        print("3. Registrar mantenimiento")
         print("4. Subir reporte técnico (MongoDB)")
         print("5. Consultar reportes técnicos")
         print("6. Salir")
@@ -19,11 +19,11 @@ def menu_tecnico(tecnico_id):
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
-            ver_equipos_asignados(tecnico_id)
+            ver_equipos()
         elif opcion == "2":
-            registrar_mantenimiento_preventivo(tecnico_id)
+            actualizar_equipo(equipo_id,campo,nuevo_valor)
         elif opcion == "3":
-            registrar_mantenimiento_correctivo(tecnico_id)
+            registrar_mantenimiento(equipo_id,descripcion,tipo,tecnico_id)
         elif opcion == "4":
             subir_reporte_tecnico(tecnico_id)
         elif opcion == "5":

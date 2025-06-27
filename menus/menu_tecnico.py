2fbf9b6b6cb8cc68aa55ca73d665ae8e9f3b4a76
sofia0911 <sofia.hosorio@udea.edu.co>
2025-06-27 12:35:30 -0500

    Update menu_tecnico.py

--- menus/menu_tecnico.py
+++ menus/menu_tecnico.py
@@ -1,8 +1,9 @@
 #Se realiza la importación de las funciones necesarias para el funcionamiento del menu del técnico
 
-from funciones.reportes_mongo import subir_reporte_tecnico, consultar_reportes
-from funciones.mantenimientos import registrar_mantenimeinto
+from funciones.reportes_mongo import reporte_tecnico, consultar_reportes
+from funciones.mantenimientos import registrar_mantenimiento
 from funciones.equipos import ver_equipos, actualizar_equipo 
+from funciones.carga_json_mongo import cargar_reportes_completos_json
 
 #Este menu permite al técnico realizar diversas acciones relacionadas con el mantenimiento de equipos y la gestión de reportes técnicos.
 def menu_tecnico(tecnico_id):
@@ -25,7 +26,18 @@ def menu_tecnico(tecnico_id):
         elif opcion == "3":
             registrar_mantenimiento(equipo_id,descripcion,tipo,tecnico_id)
         elif opcion == "4":
-            subir_reporte_tecnico(tecnico_id)
+            print("\n--- Generar Reporte Técnico ---")
+            print("\n--- Cargar archivo de reporte ---")
+
+            opcion_reporte=input("Selecciona una opción:\n1. Subir reporte técnico\n2. Subir reporte técnico con bitácora\n")
+            
+            if opcion_reporte == "1":
+                reporte_tecnico(tecnico_id)
+            elif opcion_reporte == "2":
+                cargar_reportes_completos_json()
+                print("Subiendo reporte técnico del documento json a MongoDB...")
+                
+
         elif opcion == "5":
             consultar_reportes()
         elif opcion == "6":
